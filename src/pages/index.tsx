a449039a54c3a057ef887215a5e02c8007742e50
JamesRomero2 <haymeemotoh22@gmail.com>
2023-05-03 14:05:52 +0800

    Functionality In Progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,38 @@
 import Head from 'next/head';
-import { useState } from 'react';
+import Image from 'next/image';
+import React from 'react';
+
+interface form {
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string
+}
+
 
 export default function Home() {
-  const [firstName, setFirstName] = useState<string>('');
-  const [lastName, setLastName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('')
+
+  const [formState, setFormState] = React.useState<form>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
+
+  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.name;
+    setFormState({
+      ...formState,
+      [e.currentTarget.name]: value
+    });
+  }
+
+  const submitButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    console.log("Hello");
+  }
+
   return (
     <>
       <Head>
@@ -40,31 +67,23 @@ export default function Home() {
             <p className='font-normal'><span className='font-bold'>Try it free 7 days </span>then $20/mo. thereafter</p>
           </div>
           <form action="" className='w-full bg-white rounded-lg overflow-hidden p-8 flex flex-col gap-5 drop-shadow-md'>
-            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md">
-              <label htmlFor="">
-
-              </label>
-              <input type="text" placeholder="First Name" className='w-full !outline-none'/>
+            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md flex flex-row">
+              <input type="text" placeholder="First Name" className='w-full !outline-none' onChange={handleInputChange} value={formState.firstName} name='firstName'/>
+              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 hidden'/>
             </div>
-            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md">
-              <label htmlFor="">
-
-              </label>
-              <input type="text" placeholder="Last Name" className='w-full !outline-none'/>
+            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md flex flex-row">
+              <input type="text" placeholder="Last Name" className='w-full !outline-none' onChange={handleInputChange} value={formState.lastName} name='lastName'/>
+              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 hidden'/>
             </div>
-            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md">
-              <label htmlFor="">
-
-              </label>
-              <input type="email" placeholder="Email Address" className='w-full !outline-none'/>
+            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md group flex flex-row">
+              <input type="email" placeholder="Email Address" className='w-full !outline-none' onChange={handleInputChange} value={formState.email} name='email'/>
+              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 hidden'/>
             </div>
-            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md">
-              <label htmlFor="">
-
-              </label>
-              <input type="password" placeholder="Password" className='w-full !outline-none'/>
+            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md flex flex-row">
+              <input type="password" placeholder="Password" className='w-full !outline-none' onChange={handleInputChange} value={formState.password} name='password'/>
+              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 hidden'/>
             </div>
-            <button className='bg-primary-green text-white p-5 rounded-lg border-b-neutral-gray-blue border-b-4 drop-shadow-md font-medium tracking-wider'>CLAIM YOUR FREE TRIAL</button>
+            <button className='bg-primary-green text-white p-5 rounded-lg border-b-neutral-gray-blue border-b-4 drop-shadow-md font-medium tracking-wider' onClick={submitButton}>CLAIM YOUR FREE TRIAL</button>
             <p className='text-xs text-neutral-gray-blue text-center'>By clicking the button, you are agreeing to our <a href="" className='text-xs font-medium text-primary-red'>Terms and Services</a></p>
           </form>
         </section>
