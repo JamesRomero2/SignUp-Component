cafbcc807021de997e63175b89126a29f15f1e75
JamesRomero2 <haymeemotoh22@gmail.com>
2023-05-04 11:46:23 +0800

    Functionality Done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
-interface form {
+interface IForm {
   firstName: string,
   lastName: string,
   email: string,
   password: string
 }
 
-
 export default function Home() {
 
-  const [formState, setFormState] = React.useState<form>({
+  const [formState, setFormState] = useState<IForm>({
     firstName: "",
     lastName: "",
     email: "",
@@ -20,7 +19,7 @@ export default function Home() {
   });
 
   const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.name;
+    const value = e.currentTarget.value;
     setFormState({
       ...formState,
       [e.currentTarget.name]: value
@@ -30,7 +29,18 @@ export default function Home() {
   const submitButton = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
 
-    console.log("Hello");
+    Object.keys(formState).map(key => {
+      const elementObject : HTMLElement | null = document.getElementById(key);
+      const imageErrorObject  : HTMLElement | null = document.getElementById(key + "-error");
+
+      if (formState[key as keyof IForm].length === 0) {
+        elementObject!.className = 'border-primary-red border-2 focus-within:border-neutral-dark-blue border-solid p-3 rounded-md flex flex-row ';
+        imageErrorObject!.className = "visible";
+      } else {
+        elementObject!.className = 'focus-within:border-neutral-dark-blue border-solid p-3 rounded-md flex flex-row border-neutral-gray-blue border';
+        imageErrorObject!.className = "invisible";
+      }
+    });
   }
 
   return (
@@ -67,21 +77,21 @@ export default function Home() {
             <p className='font-normal'><span className='font-bold'>Try it free 7 days </span>then $20/mo. thereafter</p>
           </div>
           <form action="" className='w-full bg-white rounded-lg overflow-hidden p-8 flex flex-col gap-5 drop-shadow-md'>
-            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md flex flex-row">
-              <input type="text" placeholder="First Name" className='w-full !outline-none' onChange={handleInputChange} value={formState.firstName} name='firstName'/>
-              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 hidden'/>
+            <div className={`focus-within:border-neutral-dark-blue border-solid p-3 rounded-md flex flex-row border-neutral-gray-blue border`} id='firstName'>
+              <input type="text" placeholder="First Name" className='w-full !outline-none' onChange={handleInputChange} value={formState.firstName} name='firstName' />
+              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 invisible' id='firstName-error'/>
             </div>
-            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md flex flex-row">
-              <input type="text" placeholder="Last Name" className='w-full !outline-none' onChange={handleInputChange} value={formState.lastName} name='lastName'/>
-              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 hidden'/>
+            <div className={`focus-within:border-neutral-dark-blue border-solid p-3 rounded-md flex flex-row border-neutral-gray-blue border`} id='lastName'>
+              <input type="text" placeholder="Last Name" className='w-full !outline-none' onChange={handleInputChange} value={formState.lastName} name='lastName' />
+              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 invisible' id='lastName-error'/>
             </div>
-            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md group flex flex-row">
-              <input type="email" placeholder="Email Address" className='w-full !outline-none' onChange={handleInputChange} value={formState.email} name='email'/>
-              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 hidden'/>
+            <div className={`focus-within:border-neutral-dark-blue border-solid p-3 rounded-md group flex flex-row border-neutral-gray-blue border`} id='email'>
+              <input type="email" placeholder="Email Address" className='w-full !outline-none' onChange={handleInputChange} value={formState.email} name='email' />
+              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 invisible' id='email-error'/>
             </div>
-            <div className="focus-within:border-neutral-dark-blue border-solid border-neutral-gray-blue border p-3 rounded-md flex flex-row">
-              <input type="password" placeholder="Password" className='w-full !outline-none' onChange={handleInputChange} value={formState.password} name='password'/>
-              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 hidden'/>
+            <div className={`focus-within:border-neutral-dark-blue border-solid p-3 rounded-md flex flex-row border-neutral-gray-blue border`} id='password'>
+              <input type="password" placeholder="Password" className='w-full !outline-none' onChange={handleInputChange} value={formState.password} name='password' />
+              <Image src={'/icon-error.svg'} alt='Icon Error' width={25} height={25} className='ml-3 invisible' id='password-error'/>
             </div>
             <button className='bg-primary-green text-white p-5 rounded-lg border-b-neutral-gray-blue border-b-4 drop-shadow-md font-medium tracking-wider' onClick={submitButton}>CLAIM YOUR FREE TRIAL</button>
             <p className='text-xs text-neutral-gray-blue text-center'>By clicking the button, you are agreeing to our <a href="" className='text-xs font-medium text-primary-red'>Terms and Services</a></p>
